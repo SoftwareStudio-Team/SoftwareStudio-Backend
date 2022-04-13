29f00d8135fcc3558e0d279dcd80b276f02266ca
Phukphoom Taphrae <phoom0529@gmail.com>
2022-04-13 23:03:31 +0700

    feat: try to create api/Auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+using Backend.Models;
+using Backend.Services;
+
+namespace Backend.Controllers;
+
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAccountService _accountService;
+
+    public AuthController(IAccountService accountService)
+    {
+        this._accountService = accountService;
+    }
+
+    [HttpPost("login")] // POST api/Auth/login
+    public ActionResult<Account> Post([FromBody] LoginForm loginForm)
+    {
+        /* implement login logic here */
+        return NoContent();
+    }
+
+    [HttpGet("logout")] // GET api/Auth/logout
+    public ActionResult Get()
+    {
+        /* implement logout logic here */
+        return NoContent();
+    }
+}
---
+++ Models/LoginForm.cs
@@ -0,0 +1,8 @@
+namespace Backend.Models
+{
+    public class LoginForm
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
