490b937b82eca9948f89f7fae421973e7c5ac029
Phukphoom TP <phukphoom@EsitoneX-MBA.local>
2022-04-13 16:45:13 +0700

    feat: add username and password field in Account

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,6 +9,12 @@ namespace Backend.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = String.Empty;
+        
+        [BsonElement("username")]
+        public string Username { get; set; }
+
+        [BsonElement("password")]
+        public string Password { get; set; }
 
         [BsonElement("firstName")]
         public string FirstName { get; set; }
--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,4 +1,5 @@
 using MongoDB.Driver;
+using System.Security.Cryptography;
 
 using Backend.Models;
 
